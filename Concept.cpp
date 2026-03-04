50ebc36a8002228a4ef5f76b61de83492f1bb6cb
Cryptofun/junior <61570493+Argsssssss@users.noreply.github.com>
2026-03-04 11:38:19 +0700

    Concept.cpp

--- Concept.cpp
+++ Concept.cpp
@@ -1,36 +1,72 @@
 #include <iostream>
 #include <vector>
-
+#include <chrono>
+#include <thread>
 using namespace std;
+#if defined(_WIN32) || defined(_WIN64) 
+    #define WINDOWS
+    #include <conio.h>
+
+#elif defined(unix)
+    #define UNIX
+    char getch(void);
+#else
+    #error "Platform is not supported"
+#endif
+
+void clear (void) {
+    #ifdef WINDOWS
+        printf("\033[2J");
+        printf("\033[0;0f");
+    #else
+        system("clear");
+    #endif
+}
+
+#ifdef UNIX
+    char getch (void) {
+        char ch;
+        system("stty raw");
+        ch = getchar();
+        system("stty cooked");
+        return ch;
+    }
+#endif
+
+
 bool game();
 int start();
-
 int main(){
     start();
     return 0;
 }
+void print_searting(string pick, int ){
 
+}
 bool game(){
-    // vector<char,char>client(50,50,NULL);
-    // vector<char>snake(100);
-    // snake[0] = '*';
-    // while(true){
-    //     for (size_t i = 0; i < 50; i++)
-    //     {
-    //         for (size_t j = 0; j < 50; j++)
-    //         {
-                
-    //         }
-            
-    //     }
-           
-    // }
+    vector<vector<char>>map(20,vector<char>(20,'Z'));
+    vector<char>snake(40,'*');
+    snake[0] = '*';
+    while(true){
+        for (size_t i = 0; i < 20; i++)
+        {
+            for (size_t j = 0; j < 20; j++)
+            {
+                cout << map[i][j] << ' ';
+            }
+            cout << endl;
+        }
+        this_thread::sleep_for(chrono::milliseconds(500));
+        clear();
+    }
     return 0;
 }
 
 int start(){
+    int client[2]{10,10}; // Размер карты
+    int speed_snake = 500; // Скорость, по факту тайминг обновления карты в МС
     menu:
-        system("clear");
+        clear();
         string pick[] = {
             "--------Select--------\n",
             "|      1 - Start     |\n",
@@ -46,50 +82,57 @@ int start(){
                 cout << pick[i][j];
             }
         }
-        int select;
-        cin >> select;
+        
+        char select = 0;
+        select = getch();
         switch (select)
         {
-            case 1: goto game;
-            case 2: goto searting;
-            case 3: exit;
+            case '1': goto game;
+            case '2': goto searting;
+            case '3': exit;
             default: break;
         };
 
     exit:return 0;
 
     game:
-        system("clear");
+        clear();
         cout << "========Welcome========\n";
         game();
 
         goto menu;
 
     searting:
-        system("clear");
+        clear();
         string pick_searting[] = {
-            "------------Searting-----------\n",
-            "|      1 - Screen Resolution   |\n",
-            "|      2 - Velocity            |\n",
-            "|      3 - Exit                |\n",
-            "--------------------------------\n",
+            "-------------Searting------------\n",
+            "|  1 - Screen Resolution %d x %d|\n",
+            "|  2 - Velocity          %d     |\n",
+            "|  3 - Exit                     |\n",
+            "---------------------------------\n",
             "U pick: "
             };
-        for (size_t i = 0; i < 6; i++)
-        {
-            for (size_t j = 0; j < pick_searting[i].size(); j++)
-            {
-                cout << pick_searting[i][j];
-            }
-            
-        }
-        int select_searting;
-        cin >> select_searting;
+
+        printf("-------------Searting------------\n");
+        printf("|  1 - Screen Resolution %d x %d|\n",client[0], client[1]);
+        printf("|  2 - Velocity          %d (MS)|\n",speed_snake);
+        printf("|  3 - Exit                     |\n---------------------------------\nU pick: ");
+        char select_searting  = getch();
         switch (select_searting)
         {
-            case 1: 
-            case 2:
-            case 3: goto menu;
+            case '1': 
+                cout << "\nEnter X: ";
+                cin >> client[0];
+                cout << endl;
+                cout << "Enter Y: ";
+                cin >> client[1];
+                goto searting;
+            case '2':
+                cout << "\nEnter the speed parameter: ";
+                cin >> speed_snake;
+                goto searting;
+            case '3': goto menu;
+            default: goto searting;
         };
 
     return 0;
