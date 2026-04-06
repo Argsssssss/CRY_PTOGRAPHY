e1cda63605de40beb5e3b33819049d3e0716e1f5
Cryptofun.jr <61570493+Argsssssss@users.noreply.github.com>
2026-04-06 15:21:27 +0700

    ECC.cpp

--- ECC.cpp
+++ ECC.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
-void ecc();
 
-struct Point { int x; int y; };
+void ecc();
+void visual_points();
+
+struct Point { int *x = {new int [255]}; int *y = {new int [255]};};
+
+int size_P(Point point){
+    for(int i = 0; ; i++){
+        if(point.x[i + 1] == 0 && point.y[i + 1] == 0){
+            return i + 1;
+        }
+    }
+    return 0;
+}
 
 int degree_mod(int ground, int degree, int mod){
     int buff = 1;
     for(int i = 0; i < degree;i++){
         buff *= ground;
-        buff % mod;
+        buff %= mod;
     }
     return buff;
 }
+
 int negative_numb(int numb, int mod){
     if(numb < 0){
         while (numb < 0)
@@ -24,6 +36,7 @@ int negative_numb(int numb, int mod){
     }
     return numb;
 }
+
 int inverse_numb_on_map_galua_GF(int numb, int mod){
     int x[255] = {1,0};
     int i = 2, b = mod, r, q;
@@ -44,27 +57,48 @@ int inverse_numb_on_map_galua_GF(int numb, int mod){
     }
     return 0;
 }
+
 bool test_singulyar(int a, int b, int mod){
     // 4a^3 + 27b^2 mod ??? != 0
     if((4 * degree_mod(a, 3, mod) + 27 * degree_mod(b, 2, mod)) % mod != 0){
-        return true;
-    }else{
         return false;
+    }else{
+        return true;
     }
     return 0;
 }
-Point generate_points(int a, int b, int mod){
-    if (test_singulyar(a, b, mod) == false) return;
-    
-    
-    Point pt;
 
+Point generate_points(int a, int b, int mod){
+    Point pt;
+    if (test_singulyar(a, b, mod) == true){ cout << "Singulyar" << endl; return pt;}
+    // y^2 = x^3 + ax + b % mod ???
+
+    for(int i = 0, x = 0, y = 0; i < mod; i++){
+        for(int j = 0; j < mod; j++){
+            if(degree_mod(j,2,mod) == (degree_mod(i,3,mod) + a * i + b) % mod){
+                pt.x[x] = i;
+                pt.y[y] = j;
+                x++;
+                y++;
+            }
+        }
+    }
     return pt;   
 }
 
 
 int main(void){
-    cout << test_singulyar(2,2,7);
+    Point qwe;
+    int a = 2, b = 1, mod = 13;
+
+    qwe = generate_points(a, b, mod);
+    if (test_singulyar(a, b, mod) == true) return 0;
+    int size = size_P(qwe);
+    cout << "Format - {x, y} ||| " << "O{∞, ∞} ";
+    for(int i = 0; i < size; i++){
+        cout <<  i + 1  << "{" <<  qwe.x[i] << ", " << qwe.y[i] << "}, ";
+    }
+    
     // ecc();
     return 0;
 }
